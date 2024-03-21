f1521de0f75f52dd794c072ce997d28a99498a45
Shanmuka Srinivas <shanmuka srinivasa>
2024-03-21 17:11:06 +0530

    Headder Completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Logo from "../photos/logo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div>
+          <img src={Logo} alt="" />
+        </div>
+        <div className="links">
+          <a href="/">Home</a>/<a href="/">About</a>/<a href="/">Services</a>/
+          <a href="/">FAQs</a>/
+          <a href="/">Contact</a>
+        </div>
+        <div className="login">
+            <button type="button">Login</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
